77b9299dcc910ad67ce1239904a66481e210a1df
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-03-26 16:42:36 +0100

    bug hunt - bills

--- Billed-app-FR/src/containers/Dashboard.js
+++ Billed-app-FR/src/containers/Dashboard.js
@@ -81,7 +81,7 @@ export default class {
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
     const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
-    $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
+    $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><object data=${billUrl} type="" width=${imgWidth} height="auto"><a href="${billUrl}">Télécharger le contenu</a></object></div>`)
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
